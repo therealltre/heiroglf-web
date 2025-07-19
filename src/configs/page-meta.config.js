e5230ed106b409a35332f07f83404ebd52951666
Tre <therealltre@gmail.com>
2025-07-19 17:59:31 +0000

    update: favicon

--- src/configs/page-meta.config.js
+++ src/configs/page-meta.config.js
@@ -1,7 +1,7 @@
 const pageMeta = {
-    title: 'HEIROGLF - The Ultimate Next & TypeScript Web Template',
+    title: 'HEIROGLF - Heiroglf is a next-generation Pan-African cryptocurrency exchange platform.',
     description:
-        'Admin Template and Starter Kit with React, Tailwind Components and Next.js',
+        'Heiroglf is a next-generation Pan-African cryptocurrency exchange, redefining finance for the continent. Built for Africa, by Africa, Heiroglf empowers users to trade, save, and invest securely in digital assets. Our mission is to bridge Africa to the global decentralized economy, providing fast, secure, and user-friendly access to cryptocurrencies for everyone across the continent.',
     icons: {
         icon: '/favicon.ico',
     },
