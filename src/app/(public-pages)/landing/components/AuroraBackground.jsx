73b992f7c0b8cf812b946f2585ff1e43abfa2268
Tre <therealltre@gmail.com>
2025-07-21 14:33:07 +0000

    update: aurora background

--- src/app/(public-pages)/landing/components/AuroraBackground.jsx
+++ src/app/(public-pages)/landing/components/AuroraBackground.jsx
@@ -11,7 +11,7 @@ const AuroraBackground = ({
     return (
         <div
             className={classNames(
-                'relative flex flex-col items-center justify-center bg-black/10 backdrop-blur-3xl transition-bg',
+                'relative flex flex-col items-center justify-center bg-[#B1743C] dark:bg-[#060606]  transition-bg',
                 className,
             )}
             {...props}
@@ -24,27 +24,27 @@ const AuroraBackground = ({
             >
                 <div
                     className={classNames(
-                        `
-            [--aurora:radial-gradient(100%_100%_at_50%_0%,#3B2F2F_0%,#5C4033_33%,#8B4513_66%,#000000_100%)]
-            [--transparent:rgba(0,0,0,0)]
-            [background-image:var(--aurora)]
+                        `[--dark-gradient:repeating-linear-gradient(100deg,var(--color-black)_0%,var(--color-black)_7%,var(--transparent)_10%,var(--transparent)_12%,var(--color-black)_16%)]
+            [--aurora:repeating-linear-gradient(100deg,#8B4513_0%,#A0522D_15%,#B1743C_20%,#D2B48C_25%,#A0522D_30%)]
+            [--transparent:rgba(255,255,255,0)]
+            [background-image:var(--dark-gradient),var(--aurora)]
             [background-size:300%,_200%]
             [background-position:50%_50%,50%_50%]
-            filter blur-[30px]
-            after:content-[""] after:absolute after:inset-0 after:[background-image:var(--aurora)]
+            filter blur-[50px]
+            after:content-[""] after:absolute after:inset-0 after:[background-image:var(--dark-gradient),var(--aurora)]
+            after:dark:[background-image:var(--dark-gradient),var(--aurora)]
             after:[background-size:200%,_100%]
-            after:[background-attachment:fixed]
-            animate-aurora
+            animate-aurora after:[background-attachment:fixed]
             pointer-events-none
             absolute -inset-[10px] opacity-50 will-change-transform`,
                         showRadialGradient &&
-                            `[mask-image:radial-gradient(ellipse_at_100%_0%,black_10%,var(--transparent)_50%)]`,
+                        `[mask-image:radial-gradient(ellipse_at_100%_0%,black_10%,var(--transparent)_70%)]`,
                     )}
                 ></div>
             </div>
             {children}
         </div>
     );
-};
+}
 
 export default AuroraBackground;
